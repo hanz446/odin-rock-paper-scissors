03bce7aa82bb7303d1840b5a8ec87b984d14ec8d
Hanz Inovejas <266005771+hhinovejas22-cyber@users.noreply.github.com>
2026-05-10 18:06:15 +0800

    JS: Add playRound function

--- script.js
+++ script.js
@@ -1,5 +1,45 @@
 
-const choices = ["Rock", "Paper", "Scissors"];
+let humanScore = 0;
+let computerScore = 0;
+
+const rock = {
+    self: "Rock",
+    winsTo: "Scissors",
+    losesTo: "Paper"
+};
+const paper = {
+    self: "Paper",
+    winsTo: "Rock",
+    losesTo: "Scissors"
+};
+const scissors = {
+    self: "Scissors",
+    winsTo: "Paper",
+    losesTo: "Rock"
+};
+const selection = {
+    rock: rock,
+    paper: paper,
+    scissors: scissors
+};
+const choices = [rock, paper, scissors];
+
+function playRound(humanChoice, computerChoice) {
+
+    switch (humanChoice.self) {
+        case computerChoice.losesTo:
+            humanScore++;
+            console.log(`You win! ${humanChoice.self} beats ${computerChoice.self}!`);
+            break;
+        case computerChoice.winsTo:
+            computerScore++;
+            console.log(`You lose! ${computerChoice.self} beats ${humanChoice.self}!`);
+            break;
+        default:
+            console.log("It's a draw!")
+    }
+    console.log(`Human: ${humanScore} Computer: ${computerScore}`)
+}
 
 function getComputerChoice() {
     let computerChoice = choices[Math.floor(Math.random() * 3)];
@@ -8,5 +48,11 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     let humanChoice = window.prompt("Choose your weapon! [Rock, Paper, Scissors]");
-    return humanChoice
+    humanChoice = humanChoice.trim().toLowerCase();
+    return selection[humanChoice]
 }
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
