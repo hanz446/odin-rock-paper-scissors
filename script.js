6feca2d50cc270dd3a35f0c8e274687e91940323
Hanz Inovejas <266005771+hhinovejas22-cyber@users.noreply.github.com>
2026-05-14 11:52:26 +0800

    Create game UI

--- script.js
+++ script.js
@@ -1,74 +1,190 @@
 
 const handSelection = {
     rock: {
+        symbol: "👊",
         self: "Rock",
         winsTo: "Scissors",
         losesTo: "Paper"
     },
     paper: {
+        symbol: "✋",
         self: "Paper",
         winsTo: "Rock",
         losesTo: "Scissors"
     },
     scissors: {
+        symbol: "✌️",
         self: "Scissors",
         winsTo: "Paper",
         losesTo: "Rock"
     }
 };
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
+const body = document.querySelector("body");
+const heading = document.querySelector("h1")
+const gameDisplay = document.getElementById("game-display");
+const playerScoreDisplay = document.getElementById("player-score");
+const compScoreDisplay = document.getElementById("comp-score");
+const compChoiceDisplay = document.getElementById("comp-choice-display");
+const resultDisplay = document.getElementById("result-display");
+const btnContainer = document.getElementById("button-container");
+const selectionBtns = document.querySelectorAll(".selection");
+const playBtn = document.getElementById("play-game");
 
-    for (let i = 0; i <= 4; i++) {
-        let roundWinner = playRound(getHumanChoice(), getComputerChoice())
-        roundWinner ? (roundWinner === "human" ? humanScore++ : computerScore++) : null;
-        displayScore(humanScore, computerScore)
+let ongoing = false;
+let genSymbol = symbolGenerator();
+
+playBtn.addEventListener("click", playGame)             // Also acts as reset button
+
+function playGame(event) {
+    let playerScore = 0;
+    let compScore = 0;
+
+    // Toggles UI
+    gameDisplay.classList.toggle("ongoing");
+    body.classList.toggle("ongoing");
+    heading.classList.toggle("ongoing");
+    playBtn.classList.toggle("ongoing");
+
+    // Starts or resets the game
+    if (ongoing) {
+        resetGame();
+    } else {
+        let animateIntervalID;
+        startGame();
     }
 
-    displayWinner(humanScore, computerScore);
-}
+    function startGame() {
+        ongoing = true;
+        btnContainer.addEventListener("click", calculateResult);
 
-function playRound(humanChoice, computerChoice) {
-    let roundWinner = null;
-    switch (humanChoice.self) {
-        case computerChoice.losesTo:
-            roundWinner = "human";
-            console.log(`You win! ${humanChoice.self} beats ${computerChoice.self}!`);
-            break;
-        case computerChoice.winsTo:
-            roundWinner = "computer";
-            console.log(`You lose! ${computerChoice.self} beats ${humanChoice.self}!`);
-            break;
-        default:
-            console.log("It's a draw!");
-    }
-    return roundWinner;
-}
-
-function getComputerChoice() {
-    const choices = ["rock", "paper", "scissors"]
-    const computerChoice = choices[Math.floor(Math.random() * 3)];
-    return handSelection[computerChoice];
-}
-
-function getHumanChoice() {
-    let humanChoice = window.prompt("Choose your weapon! [Rock, Paper, Scissors]").trim().toLowerCase();
-
-    while (handSelection[humanChoice] === undefined) {
-        humanChoice = window.prompt("Choose a valid weapon! [Rock, Paper, Scissors]").trim().toLowerCase();
+        animateIntervalID = setInterval(() => compChoiceDisplay.textContent = genSymbol.next().value, 500); // Start compChoiceDisplay animation
+        setTimeout(updatePlayBtn, 320);
     }
 
-    return handSelection[humanChoice];
+    function resetGame() {
+        ongoing = false;
+
+        updateScore("reset");
+        updatePlayBtn();
+
+        compChoiceDisplay.textContent = "✌️";
+        setTimeout(() => resultDisplay.textContent = "First to five wins!", 300);
+
+        gameDisplay.classList.remove("win")
+        gameDisplay.classList.remove("lose")
+        compChoiceDisplay.classList.remove("win")
+        compChoiceDisplay.classList.remove("lose")
+
+        clearInterval(animateIntervalID);                                             // Stops animation
+        const currentState = selectionBtns[0].className.trim().split(' ').pop();      // Checks for current button state
+        selectionBtns.forEach(btn => btn.classList.replace(currentState, "default")); // Recolors each selection button to default
+    }
+    
+    function isOver(playerScore, compScore) {
+        return playerScore >= 5 || compScore >=5;
+    }
+
+    function gameOver() {
+        btnContainer.removeEventListener("click", calculateResult);
+
+        let gameWinner = playerScore === 5 ? "player" : "computer";
+        let result = gameWinner === "player" ? "Congratulations! You win!" : "Aww, better luck next time!";
+
+        resultDisplay.textContent = result;
+
+        if (gameWinner === "player") {
+            gameDisplay.classList.add("win");
+            compChoiceDisplay.classList.add("win");
+        } else {
+            gameDisplay.classList.add("lose");
+            compChoiceDisplay.classList.add("lose");
+        }
+    }
+
+    function getPlayerChoice(event) {
+        playerChoice = handSelection[event.target.id];
+        return playerChoice;
+    }
+
+    function getCompChoice() {
+        const choices = ["rock", "paper", "scissors"]
+        const compChoice = handSelection[choices[Math.floor(Math.random() * 3)]];
+        clearInterval(animateIntervalID);
+        compChoiceDisplay.textContent = compChoice.symbol;
+        return compChoice;
+    }
+
+    function calculateResult(event) {
+        const playerChoice = getPlayerChoice(event)
+        const compChoice = getCompChoice();
+        let roundWinner = null;
+
+        switch (playerChoice.self) {
+            case compChoice.losesTo:
+                roundWinner = "player";
+                resultDisplay.textContent = `You win! ${playerChoice.self} beats ${compChoice.self}!`;
+                break;
+            case compChoice.winsTo:
+                roundWinner = "computer";
+                resultDisplay.textContent = `You lose! ${compChoice.self} beats ${playerChoice.self}!`;
+                break;
+            default:
+                resultDisplay.textContent = "It's a draw!";
+        }
+        updateSelectionColor(roundWinner);
+        updateScore(roundWinner);
+
+        if (isOver(playerScore, compScore)) {
+            gameOver();
+        }
+    }
+
+    function updateSelectionColor(roundWinner) {
+        const currentState = selectionBtns[0].className.trim().split(' ').pop();
+        console.log(selectionBtns[0].className);
+        switch (roundWinner) {
+            case "player":
+                selectionBtns.forEach(btn => btn.classList.replace(currentState, "win"));
+                break;
+            case "computer":
+                selectionBtns.forEach(btn => btn.classList.replace(currentState, "lose"));
+                break;
+            default:
+                selectionBtns.forEach(btn => btn.classList.replace(currentState, "default"));
+        }
+    }
+
+    function updateScore(roundWinner) {
+        if (roundWinner) {
+            switch (roundWinner) {
+                case "player":
+                    playerScore++;
+                    break;
+                case "computer":
+                    compScore++;
+                    break;
+                case "reset":
+                    playerScore = 0;
+                    compScore = 0;
+                    break;
+            }
+            playerScoreDisplay.textContent = `Player: ${playerScore}`;
+            compScoreDisplay.textContent = `Computer: ${compScore}`;
+        }
+    }
+    
+    function updatePlayBtn() {
+        ongoing ? playBtn.textContent = "Reset" : playBtn.textContent = "Play Game";
+    }
 }
 
-function displayScore(humanScore, computerScore) {
-    console.log(`Human: ${humanScore} Computer: ${computerScore}`);
+function* symbolGenerator() {
+    const symbols = ["👊", "✋", "✌️"];
+    let index = 0;
+    while (true) {
+        yield symbols[index];
+        index = (index + 1) % 3;
+    }
 }
 
-function displayWinner(humanScore, computerScore) {
-    humanScore == computerScore ? console.log("It's a Draw!") : (humanScore > computerScore ? console.log("You win! Congratulations!") : console.log("You lose! Try again!"));
-}
-
-playGame()
