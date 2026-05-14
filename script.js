ddd4d6187edb7aaaf4e4652b782d5782ba97effe
Hanz Inovejas <266005771+hhinovejas22-cyber@users.noreply.github.com>
2026-05-14 13:44:22 +0800

    Refactor script file

--- script.js
+++ script.js
@@ -1,190 +1,172 @@
+class RpsGame {
+    constructor() {
+        this.body = document.querySelector("body");
+        this.heading = document.querySelector("h1")
+        this.gameDisplay = document.getElementById("game-display");
+        this.playerScoreDisplay = document.getElementById("player-score");
+        this.compScoreDisplay = document.getElementById("comp-score");
+        this.compChoiceDisplay = document.getElementById("comp-choice-display");
+        this.resultDisplay = document.getElementById("result-display");
+        this.btnContainer = document.getElementById("button-container");
+        this.selectionBtns = document.querySelectorAll(".selection");
+        this.playBtn = document.getElementById("play-game");
 
-const handSelection = {
-    rock: {
-        symbol: "👊",
-        self: "Rock",
-        winsTo: "Scissors",
-        losesTo: "Paper"
-    },
-    paper: {
-        symbol: "✋",
-        self: "Paper",
-        winsTo: "Rock",
-        losesTo: "Scissors"
-    },
-    scissors: {
-        symbol: "✌️",
-        self: "Scissors",
-        winsTo: "Paper",
-        losesTo: "Rock"
-    }
-};
+        this.playBtn.addEventListener("click", (event) => this.handleClick(event));
+        this.btnContainer.addEventListener("click", (event) => this.handleRound(event));
 
-const body = document.querySelector("body");
-const heading = document.querySelector("h1")
-const gameDisplay = document.getElementById("game-display");
-const playerScoreDisplay = document.getElementById("player-score");
-const compScoreDisplay = document.getElementById("comp-score");
-const compChoiceDisplay = document.getElementById("comp-choice-display");
-const resultDisplay = document.getElementById("result-display");
-const btnContainer = document.getElementById("button-container");
-const selectionBtns = document.querySelectorAll(".selection");
-const playBtn = document.getElementById("play-game");
+        this.handSelection = {
+            0: {
+                symbol: "👊",
+                self: "Rock",
+                winsTo: "Scissors",
+                losesTo: "Paper"
+            },
+            1: {
+                symbol: "✋",
+                self: "Paper",
+                winsTo: "Rock",
+                losesTo: "Scissors"
+            },
+            2: {
+                symbol: "✌️",
+                self: "Scissors",
+                winsTo: "Paper",
+                losesTo: "Rock"
+            }
+        }
 
-let ongoing = false;
-let genSymbol = symbolGenerator();
-
-playBtn.addEventListener("click", playGame)             // Also acts as reset button
-
-function playGame(event) {
-    let playerScore = 0;
-    let compScore = 0;
-
-    // Toggles UI
-    gameDisplay.classList.toggle("ongoing");
-    body.classList.toggle("ongoing");
-    heading.classList.toggle("ongoing");
-    playBtn.classList.toggle("ongoing");
-
-    // Starts or resets the game
-    if (ongoing) {
-        resetGame();
-    } else {
-        let animateIntervalID;
-        startGame();
+        this.ongoing = false;
+        this.playerScore = 0;
+        this.compScore = 0;
+        this.genSymbol = RpsGame.symbolGenerator();
+        this.animateIntervalID = null;
     }
 
-    function startGame() {
-        ongoing = true;
-        btnContainer.addEventListener("click", calculateResult);
-
-        animateIntervalID = setInterval(() => compChoiceDisplay.textContent = genSymbol.next().value, 500); // Start compChoiceDisplay animation
-        setTimeout(updatePlayBtn, 320);
+    handleClick(event) {
+        this.toggleUI();
+        this.ongoing ? this.resetGame() : this.startGame();
     }
 
-    function resetGame() {
-        ongoing = false;
-
-        updateScore("reset");
-        updatePlayBtn();
-
-        compChoiceDisplay.textContent = "✌️";
-        setTimeout(() => resultDisplay.textContent = "First to five wins!", 300);
-
-        gameDisplay.classList.remove("win")
-        gameDisplay.classList.remove("lose")
-        compChoiceDisplay.classList.remove("win")
-        compChoiceDisplay.classList.remove("lose")
-
-        clearInterval(animateIntervalID);                                             // Stops animation
-        const currentState = selectionBtns[0].className.trim().split(' ').pop();      // Checks for current button state
-        selectionBtns.forEach(btn => btn.classList.replace(currentState, "default")); // Recolors each selection button to default
-    }
-    
-    function isOver(playerScore, compScore) {
-        return playerScore >= 5 || compScore >=5;
+    toggleUI() {
+        this.gameDisplay.classList.toggle("ongoing");
+        this.body.classList.toggle("ongoing");
+        this.heading.classList.toggle("ongoing");
+        this.playBtn.classList.toggle("ongoing");
     }
 
-    function gameOver() {
-        btnContainer.removeEventListener("click", calculateResult);
+    startGame() {
+        this.ongoing = true;
 
-        let gameWinner = playerScore === 5 ? "player" : "computer";
-        let result = gameWinner === "player" ? "Congratulations! You win!" : "Aww, better luck next time!";
+        this.selectionBtns.forEach(btn => btn.disabled = false);
+        this.animateIntervalID = setInterval(() => this.compChoiceDisplay.textContent = this.genSymbol.next().value, 500);
+        setTimeout(() => this.playBtn.textContent = "Reset", 320)
+    }
 
-        resultDisplay.textContent = result;
+    resetGame() {
+        this.ongoing = false;
+        this.playerScore = 0;
+        this.compScore = 0;
 
-        if (gameWinner === "player") {
-            gameDisplay.classList.add("win");
-            compChoiceDisplay.classList.add("win");
-        } else {
-            gameDisplay.classList.add("lose");
-            compChoiceDisplay.classList.add("lose");
+        this.playBtn.textContent = "Play Game"
+        this.compChoiceDisplay.textContent = "✌️";
+        setTimeout(() => this.resultDisplay.textContent = "First to five wins!", 300);
+
+        this.gameDisplay.classList.remove("win", "lose")
+        this.compChoiceDisplay.classList.remove("win", "lose")
+
+        this.updateScoreDisplay();
+        this.stopChoiceAnim();
+        this.updateBtnState("default");
+    }
+
+    handleRound(event) {
+        const playerChoice = this.handSelection[event.target.id];
+        const compChoice = this.handSelection[Math.floor(Math.random() * 3)];
+        const roundResult = this.evaluateResult(playerChoice, compChoice);
+
+        this.stopChoiceAnim();
+        this.compChoiceDisplay.textContent = compChoice.symbol;
+
+        this.updateBtnState(roundResult);
+        this.updateScore(roundResult);
+        this.updateScoreDisplay();
+        
+        if (this.isOver()) {
+            this.handleGameOver();
         }
     }
 
-    function getPlayerChoice(event) {
-        playerChoice = handSelection[event.target.id];
-        return playerChoice;
-    }
-
-    function getCompChoice() {
-        const choices = ["rock", "paper", "scissors"]
-        const compChoice = handSelection[choices[Math.floor(Math.random() * 3)]];
-        clearInterval(animateIntervalID);
-        compChoiceDisplay.textContent = compChoice.symbol;
-        return compChoice;
-    }
-
-    function calculateResult(event) {
-        const playerChoice = getPlayerChoice(event)
-        const compChoice = getCompChoice();
-        let roundWinner = null;
+    evaluateResult(playerChoice, compChoice) {
+        let roundResult;
 
         switch (playerChoice.self) {
             case compChoice.losesTo:
-                roundWinner = "player";
-                resultDisplay.textContent = `You win! ${playerChoice.self} beats ${compChoice.self}!`;
+                roundResult = "win";
+                this.resultDisplay.textContent = `You win! ${playerChoice.self} beats ${compChoice.self}!`;
                 break;
             case compChoice.winsTo:
-                roundWinner = "computer";
-                resultDisplay.textContent = `You lose! ${compChoice.self} beats ${playerChoice.self}!`;
+                roundResult = "lose";
+                this.resultDisplay.textContent = `You lose! ${compChoice.self} beats ${playerChoice.self}!`;
                 break;
             default:
-                resultDisplay.textContent = "It's a draw!";
+                roundResult = "default";
+                this.resultDisplay.textContent = "It's a draw!";
         }
-        updateSelectionColor(roundWinner);
-        updateScore(roundWinner);
 
-        if (isOver(playerScore, compScore)) {
-            gameOver();
+        return roundResult;
+    }
+
+    isOver() {
+        return this.playerScore === 5 || this.compScore ===5;
+    }
+
+    handleGameOver() {
+        this.selectionBtns.forEach(btn => btn.disabled = true);
+
+        if (this.playerScore === 5) {
+            this.gameDisplay.classList.add("win");
+            this.compChoiceDisplay.classList.add("win");
+            this.resultDisplay.textContent = "Congratulations! You win!";
+        } else {
+            this.gameDisplay.classList.add("lose");
+            this.compChoiceDisplay.classList.add("lose");
+            this.resultDisplay.textContent = "Aww, better luck next time!";
         }
     }
 
-    function updateSelectionColor(roundWinner) {
-        const currentState = selectionBtns[0].className.trim().split(' ').pop();
-        console.log(selectionBtns[0].className);
-        switch (roundWinner) {
-            case "player":
-                selectionBtns.forEach(btn => btn.classList.replace(currentState, "win"));
+    updateScore(roundResult) {
+        switch (roundResult) {
+            case "win":
+                this.playerScore++;
                 break;
-            case "computer":
-                selectionBtns.forEach(btn => btn.classList.replace(currentState, "lose"));
+            case "lose":
+                this.compScore++;
                 break;
-            default:
-                selectionBtns.forEach(btn => btn.classList.replace(currentState, "default"));
         }
     }
 
-    function updateScore(roundWinner) {
-        if (roundWinner) {
-            switch (roundWinner) {
-                case "player":
-                    playerScore++;
-                    break;
-                case "computer":
-                    compScore++;
-                    break;
-                case "reset":
-                    playerScore = 0;
-                    compScore = 0;
-                    break;
-            }
-            playerScoreDisplay.textContent = `Player: ${playerScore}`;
-            compScoreDisplay.textContent = `Computer: ${compScore}`;
-        }
+    updateScoreDisplay() {
+        this.playerScoreDisplay.textContent = `Player: ${this.playerScore}`;
+        this.compScoreDisplay.textContent = `Computer: ${this.compScore}`;
+    }
+
+    updateBtnState(state) {
+        const currentState = this.selectionBtns[0].className.trim().split(' ').pop();
+        this.selectionBtns.forEach(btn => btn.classList.replace(currentState, state));
+    }
+
+    stopChoiceAnim() {
+        clearInterval(this.animateIntervalID);
     }
     
-    function updatePlayBtn() {
-        ongoing ? playBtn.textContent = "Reset" : playBtn.textContent = "Play Game";
+    static *symbolGenerator() {
+        const symbols = ["👊", "✋", "✌️"];
+        let index = 0;
+        while (true) {
+            yield symbols[index];
+            index = (index + 1) % 3;
+        }
     }
 }
 
-function* symbolGenerator() {
-    const symbols = ["👊", "✋", "✌️"];
-    let index = 0;
-    while (true) {
-        yield symbols[index];
-        index = (index + 1) % 3;
-    }
-}
-
+new RpsGame();
